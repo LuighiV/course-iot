89faac15f2ffe27e0893fd428efcf7b64812fdf0
Luighi <luighiavz@gmail.com>
2022-08-21 23:18:31 -0500

    Add extra paths to project build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,6 +68,8 @@ html_show_sourcelink = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ['CNAME', 'robots.txt']
+
 # source_suffix = ['.rst', '.md']
 source_suffix = '.rst'
 
